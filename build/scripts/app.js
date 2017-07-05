3d241af6223a16185ded60f148fe949dce0a6d4e
Alexey <teos.nl@gmail.com>
2017-07-05 22:41:41 +0300

    header, main-news, gallery

--- build/scripts/app.js
+++ build/scripts/app.js
@@ -5,12 +5,16 @@ $(function () {
     $(".js-main-news-list").mCustomScrollbar();
 
     $(".js-main-news-gallery").slick({
-        arrows: false,
+        //arrows: false,
+        prevArrow: ".js-main-news-gallery-arrow-left",
+        nextArrow: ".js-main-news-gallery-arrow-right",
         dots: true,
         appendDots: ".js-main-news-gallery-dots"
     });
 
     $(".js-gallery-imgs").slick({
+        prevArrow: ".js-gallery-block-arrow-left",
+        nextArrow: ".js-gallery-block-arrow-right",
         slidesToShow: 6,
         swipeToSlide: true
     });
